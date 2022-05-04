caf3892661e82080c0bbdc392c008f2a3ab249e6
PedroFAC <pedrofacpeix@gmail.com>
2022-05-04 13:50:58 -0300

    Axios test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <h1>Hello Docker World!</h1>;
+  const [msg, setMsg] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  fetch();
+  useEffect(() => {
+    async function axiosFetch() {
+      setIsLoading(true);
+      try {
+        const { data } = await axios.get("http://localhost:8000/hello", {
+          proxy: true,
+        });
+        setMsg(data.hello);
+      } catch (error) {
+        setMsg("error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    axiosFetch();
+  }, []);
+  return <h1>{isLoading ? "Loading..." : msg}</h1>;
 }
 
 export default App;
